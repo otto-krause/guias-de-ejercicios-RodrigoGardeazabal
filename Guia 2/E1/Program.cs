8733434ea9e7f8cd3c5ae31700aeda147ca5ff79
RodrigoGardeazabal <rodrigo.e.gardeazabal@gmail.com>
2020-04-20 21:24:56 -0300

    completado

--- Guia 2/E1/Program.cs
+++ Guia 2/E1/Program.cs
@@ -7,7 +7,7 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            int num=0,opcion=1;
+            int num=0,opcion=1,cant=0,i=0,may=0;
             List<int> numeros=new List<int>();
             Console.WriteLine("ingrese numeros, si ingresa 0 ya no podra ingresar");
             while(opcion==1)
@@ -22,6 +22,18 @@ namespace E1
                     opcion=2;
                 }
             }
+            foreach(int num1 in numeros)
+            {
+                if(num1>50)
+                {
+                    may++;
+                }
+                Console.WriteLine("el numero es :"+num1);
+                cant++;
+            }
+            Console.WriteLine("la cantidad de numeros ingresados es de :"+cant);
+            Console.WriteLine("el primer numero ingresado fue: "+numeros[i]+"\nel ultimo fue:"+numeros[cant-1]);
+            Console.WriteLine("la cantidad de numeros que superan 50 es de :"+may);
         }
     }
 }
